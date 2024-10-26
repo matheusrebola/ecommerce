8b5a92c2b204dfaad39d39ce7b56404b2e15bd16
matheusrebola <matheusrebola@outlook.com>
2024-10-26 20:10:02 -0300

    feat: configuração das entidades

---
+++ backend/negative-feedbacks-service/src/entities/NegativeFeedbacks.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import type { EStatusResolucao } from "../enums/EStatusResolucao";
+
+@Entity()
+export class NegativeFeedbacks{
+  @PrimaryGeneratedColumn()
+  id!:string;
+
+  descricaoProblema!:string;
+
+  dataFeedback!:Date;
+
+  statusResolucao!:EStatusResolucao;
+}
