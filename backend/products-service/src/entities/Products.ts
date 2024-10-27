49d4893a566d40a679f5ef5252fe13d60199eb44
matheusrebola <matheusrebola@outlook.com>
2024-10-27 12:38:17 -0300

    feat: atualização dos atributos da entidade

--- backend/products-service/src/entities/Products.ts
+++ backend/products-service/src/entities/Products.ts
@@ -1,9 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { EEstoqueDisponivel } from "../enums/EEstoqueDisponivel";
+import { ECategoria } from "../enums/ECategoria";
+import { ESubCategoria } from "../enums/ESubCategoria";
 
 @Entity()
 export class Products{
   @PrimaryGeneratedColumn()
   id!:string;
-
-  
+  nomeProduto!:string;
+  descricao!:string;
+  preco!:number;
+  estoqueDisponivel!:EEstoqueDisponivel;
+  categoria!:ECategoria;
+  subCategoria!:ESubCategoria;
+  marca!:string;
+  dataInclusao!:Date;
+  tagsProduto!:string;
 }
